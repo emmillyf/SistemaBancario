e5d69b04dbabfa57778e6d2a13bb933bb3593734
Nicks-Souza <nicolas.souza990@gmail.com>
2024-04-12 16:26:02 -0300

    commitei

--- trabalhogrupo3/src/br/com/poo/g3/GrupoApplication.java
+++ trabalhogrupo3/src/br/com/poo/g3/GrupoApplication.java
@@ -1,10 +1,17 @@
 package br.com.poo.g3;
 
+import java.io.IOException;
+
+import br.com.poo.g3.io.RelatorioIO;
+
 public class GrupoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
 
+	            RelatorioIO.leitor("banco");
+		
 	}
 
 }
