42b0199e364e3da2119a1c27bbeb2428e844fec7
Emmillyf <mimig2250@gmail.com>
2024-04-15 22:51:00 -0300

    feat: correção do controller de funcionários

--- trabalhogrupo3/src/br/com/poo/g3/controllers/FuncionarioController.java
+++ trabalhogrupo3/src/br/com/poo/g3/controllers/FuncionarioController.java
@@ -5,17 +5,9 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import br.com.indiv.services.FuncionarioService;
-import br.com.indiv.util.Util;
+import br.com.poo.g3.services.FuncionarioService;
+import br.com.poo.g3.util.Util;
 
 public class FuncionarioController {
-	
-	FuncionarioService funcionarioService = new FuncionarioService();
-	static Logger logger = Util.setupLogger();
 
-	public void listarNomesFuncionarios() throws IOException {
-		List<String> funcionario = funcionarioService.listaNomesFuncionarios();
-		Util.customizer();
-		logger.log(Level.INFO, () -> funcionario + "\nLista gerada com sucesso!");
-	}
 }
